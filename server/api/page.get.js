3a0d8d2966188d6b0ccd80c7269f0ed36f1d47d1
Ellen Marie Bartling <ellenbartling@gmail.com>
2023-02-26 21:08:21 -0800

    First commit

---
+++ server/api/page.get.js
@@ -0,0 +1,18 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const page_id = process.env.NOTION_PAGE_ID;
+
+let payload = [];
+
+async function getPage() {
+  const data = await notion.pages.retrieve({ page_id });
+  console.log(data)
+  return data;
+}
+
+getPage().then((data) => {
+  payload = data;
+});
+
+export default defineEventHandler(() => payload);
