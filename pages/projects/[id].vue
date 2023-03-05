26b7a617a8421638f92f8d9a3c04cd6e0ae5f499
Ellen Marie Bartling <ellenbartling@gmail.com>
2023-03-04 16:07:20 -0800

    add styling for case studies

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,43 +1,20 @@
 <template>
     <NotionRenderer class="wrap" v-if="page" :blockMap="page" fullPage />
 </template>
-<script setup lang="ts">
-const { $notion } = useNuxtApp()
+<script setup>
+import { NotionRenderer, getPageBlocks, useGetPageBlocks } from "vue3-notion"
+// const { $notion } = useNuxtApp()
 const route = useRoute()
 
-const { data: page } = await useAsyncData("notion", () => {
-    const data = $notion.getPageBlocks(route.query.id)
-    console.log('log?', $notion, data)
-    return $notion.getPageBlocks(route.query.id)
-})
+const { data: page } = await useGetPageBlocks(route.query.id)
+// const { data: page } = await useAsyncData("notion", () => {
+//     return $notion.getPageBlocks(route.query.id)
+// })
 
 // const { data: page } = await useFetch('/api/page', {
 //     query: {
 //         id: route.query.id
 //     }
 // })
-console.log(page)
+// console.log(page)
 </script>
-<style scoped>
-.notion-text {
-    @apply text-2xl text-gray-300 font-light leading-9;
-}
-
-.notion { 
-    @apply text-white pt-28; 
-}
-
-.notion-asset-wrapper {
-    @apply w-screen !important;
-    @apply px-6;
-}
-.notion-asset-wrapper img {
-    @apply w-screen h-auto;
-    max-width: 100%;
-}
-
-.notion-asset-wrapper div {
-    @apply h-auto w-screen;
-    max-width: 95vw;
-}
-</style>
